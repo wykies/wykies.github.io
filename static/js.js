d66cf1055826b0bb9ae812902652e132ccfbfb6f
Che <43485962+c-git@users.noreply.github.com>
2022-06-12 03:04:36 -0400

    Simplify JS

--- static/js.js
+++ static/js.js
@@ -1,4 +1,4 @@
-let myIndex = 0;
+let myIndex = -1;
 
 carousel();
 
@@ -8,11 +8,8 @@ function carousel() {
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    myIndex++;
-    if (myIndex > x.length) {
-        myIndex = 1
-    }
-    x[myIndex - 1].style.display = "block";
+    myIndex = ++myIndex % x.length;
+    x[myIndex].style.display = "block";
     if (x.length > 0) { // To prevent call back on pages with no images
         setTimeout(carousel, 10000); // TODO 4: Move delay to file with configs
     }
