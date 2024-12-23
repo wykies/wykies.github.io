778f73d5e7c278a2394f81b7e2a46770ce57b8a7
One <43485962+c-git@users.noreply.github.com>
2024-12-23 14:41:01 -0500

    style: apply formatting to js file

--- static/js/banner.js
+++ static/js/banner.js
@@ -2,36 +2,35 @@ let currImgIndex = -1;
 let timerCarousel;
 let carouselImgs;
 
-
 function advanceCarousel() {
-    carouselImgs[currImgIndex].style.display = "none";
-    currImgIndex = ++currImgIndex % carouselImgs.length;
-    carouselImgs[currImgIndex].style.display = "block";
+  carouselImgs[currImgIndex].style.display = "none";
+  currImgIndex = ++currImgIndex % carouselImgs.length;
+  carouselImgs[currImgIndex].style.display = "block";
 }
 
 function onClickHandlerAdvance() {
-    clearInterval(timerCarousel);
-    advanceCarousel();
-    timerCarousel = setInterval(advanceCarousel, BANNER_TIMEOUT);
+  clearInterval(timerCarousel);
+  advanceCarousel();
+  timerCarousel = setInterval(advanceCarousel, BANNER_TIMEOUT);
 }
 
 (function setupCarousel() {
-    carouselImgs = document.getElementsByClassName(IMGS_CLASS);
-    if (carouselImgs.length <= 0) { // To prevent call back on pages with no images
-        console.log("No Images found with class: " + IMGS_CLASS);
-        return; // Do nothing no images found
-    }
+  carouselImgs = document.getElementsByClassName(IMGS_CLASS);
+  if (carouselImgs.length <= 0) { // To prevent call back on pages with no images
+    console.log("No Images found with class: " + IMGS_CLASS);
+    return; // Do nothing no images found
+  }
 
-    // Hide all images
-    for (let i = 0; i < carouselImgs.length; i++) {
-        carouselImgs[i].style.display = "none";
-    }
+  // Hide all images
+  for (let i = 0; i < carouselImgs.length; i++) {
+    carouselImgs[i].style.display = "none";
+  }
 
-    currImgIndex = carouselImgs.length - 1; // Initialize last image
-    timerCarousel = setInterval(advanceCarousel, BANNER_TIMEOUT);
-    advanceCarousel();
+  currImgIndex = carouselImgs.length - 1; // Initialize last image
+  timerCarousel = setInterval(advanceCarousel, BANNER_TIMEOUT);
+  advanceCarousel();
 
-    // Set onclick handler to advance photo
-    let container = document.getElementById(IMGS_CONTAINER_ID);
-    container.onclick = onClickHandlerAdvance;
+  // Set onclick handler to advance photo
+  let container = document.getElementById(IMGS_CONTAINER_ID);
+  container.onclick = onClickHandlerAdvance;
 })();
